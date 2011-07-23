7d1952b1a5cf7d23cdf5a07875397b25380d4cba
Henrik Bengtsson <henrikb@braju.com>
2011-07-23 00:00:00 +0000

    version 0.2.0

--- R/zzz.R
+++ R/zzz.R
@@ -4,6 +4,33 @@
 
 
 .onAttach <- function(libname, pkgname) {
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Loading/installing affxparser
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  # Load 'affxparser'
+  res <- suppressWarnings({
+    require("affxparser");
+  });
+
+  # Not installed?
+  if (!res) {
+    if (interactive()) {
+      cat("Package 'affxparser' is not available or could not be loaded. Will now try to install it from Bioconductor (requires working internet connection):\n");
+      # To please R CMD check
+      biocLite <- NULL; rm(biocLite);
+      source("http://www.bioconductor.org/biocLite.R");
+      biocLite("affxparser");
+      # Assert that the package can be successfully loaded
+      res <- require("affxparser");
+      if (!res) {
+        throw("Package 'affxparser' could not be loaded. Please install it from Bioconductor, cf. http://www.bioconductor.org/");
+      }
+    } else {
+      warning("Package 'affxparser' could not be loaded. Without it ", pkgname, " will not work. Please install it from Bioconductor, cf. http://www.bioconductor.org/");
+    }
+  }
+ 
+
   pkg <- Package(pkgname);
   assign(pkgname, pkg, pos=getPosition(pkg));
 
